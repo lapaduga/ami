f8df3ba7a86def90c041a472ffa5ab84ac3d0742
lapaduga <deniskotov29042015@gmail.com>
2020-11-18 20:01:31 +0500

    header so far

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-
+	$('.header__burger').click(function () {
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
